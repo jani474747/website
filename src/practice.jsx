ecc6767e30b301bfa57af7abc4686f8b746c5ab5
jani474747 <96036214+jani474747@users.noreply.github.com>
2022-06-13 18:53:45 +0530

    done half

---
+++ src/practice.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const practice = () => {
+  return (
+    <div>
+    <h1 style ={{
+      size : "100px"  
+      }}>Run restaurent</h1>
+    </div>
+  )
+}
+
+export default practice
